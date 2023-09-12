16e9fb766fdd3bcfeecf1b444c8a8f2b0e508f21
ecieve <essayoks@gmail.com>
2023-09-12 04:47:37 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+	/**
+	*main - Test function for positive_or_negative with a case of 0
+ * Return: Always 0
+	 */
+	int main(void)
+	{
+	    int i;
+	     i = 0;
+	    positive_or_negative(i);
+	    return (0);
+	}
